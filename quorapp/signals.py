76558cdae457caddf1f91d3ba784e9ee5ea0036a
jsihin <jishin.cheruvattil@slots.com.kw>
2025-04-11 15:39:20 +0530

    notification

--- quorapp/signals.py
+++ quorapp/signals.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save, pre_save
 from .models import Question, Answer
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-
+import json
 @receiver(post_save, sender=Question)
 def post_save_question(sender, created, instance, **kwargs):
     if created:
